abd89224e611dc2ef3d076a5acdc9eede80469fb
Alex Mirov <alexander.mirov@gmail.com>
2018-05-02 13:18:07 -0700

    Updated readme. Changed agent update script from mv files to cp files from git repo into source directory.

--- pysc2/agents/updateAgents.sh
+++ pysc2/agents/updateAgents.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mv *.py /home/alex/.local/lib/python2.7/site-packages/pysc2/agents/
-mv *.pyc /home/alex/.local/lib/python2.7/site-packages/pysc2/agents/
+cp *.py /home/alex/.local/lib/python2.7/site-packages/pysc2/agents/
+cp *.pyc /home/alex/.local/lib/python2.7/site-packages/pysc2/agents/
