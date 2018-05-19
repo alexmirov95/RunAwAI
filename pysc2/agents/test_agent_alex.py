132abf6c08d7fce150c86a96a61a44a3837ddf68
alexmirov95 <alexander.mirov@gmail.com>
2018-05-19 16:31:08 -0700

    More Modular Move Function Updated move function so that a target destination, and the move function can be called to move all units to the set target. Specified location to plug in NN API.

--- pysc2/agents/test_agent_alex.py
+++ pysc2/agents/test_agent_alex.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 import numpy
 import time
+import random
 
 from pysc2.agents import base_agent
 from pysc2.lib import actions
@@ -38,114 +39,82 @@ _SELECT_ALL = [0]
 _UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
 
 
-def get_location(pixel_y, pixel_x, mode='Single'):
-    if mode == 'Multiple':
-        raise ValueError('unimplemented for mode=Single')
-    y = pixel_y.mean()
-    x = pixel_x.mean()
-    return (x, y)
-
+def get_location(obs):
+    # get player locations
+    player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
+    player_y, player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
+    return [int(player_x.mean()), int(player_y.mean())]
 
 class MoveTest(base_agent.BaseAgent):
-  """An agent specifically for solving the MoveToBeacon map."""
+  ct = 0 # target step count
+  target = [0] * 2 # uninitialized target value
+  locations = [[int(10), int(10)], [int(60), int(15)], [int(60), int(50)], [int(10), int(50)]]
 
-  ct = 0
-  # square dancing
-  locations = [[int(10), int(10)], [int(50), int(15)], [int(50), int(50)], [int(10), int(50)]]
-  # linear movement along x axis
-  # locations = [[int(5), int(30)], [int(10), int(30)], [int(15), int(30)], [int(20), int(30)]]
+  def moveToLocation (self, obs):
+    target = self.target
+    # get current location
+    currentLocation = get_location(obs)
+    # how close the units will get to the destination before selecting a new target destination
+    distToTarget = 5
+    if (((abs(currentLocation[0] - target[0]) >= distToTarget) or 
+        (abs(currentLocation[1] - target[1]) >= distToTarget)) and 
+        (self.ct > 0)):
+      # has not reached (or at least gotten close to) destination
+      # no op
+      time.sleep(0.2)
+      return {
+        "function": actions.FunctionCall(_MOVE_SCREEN, [_NOT_QUEUED, target]),
+        "status": "MOVING_TO_TARGET"
+      }
+    else:
+      # close enough to destination for a new waypoint
+      # set new target destination waypoint
+      print("Arrived at target ", self.target)
+      time.sleep(0.2)
+      return {
+        "function": actions.FunctionCall(_MOVE_SCREEN, [_NOT_QUEUED, target]),
+        "status": "ARRIVED_AT_TARGET"
+      }
 
-  target = [-1] * 2 # uninitialized target value
+  def setTargetDestination (self, coordinates):
+    """
+      Sets the target destination to an x and y tuple in coordinates
+    """
+    x = coordinates[0]
+    y = coordinates[1]
+    if ((x > 64) or (0 > x) or 
+      (y > 64) or (0 > y)):
+      print("Invalid target coordinates.")
+    self.target = [x, y]
 
   def step(self, obs):
     super(MoveTest, self).step(obs)
-    if _MOVE_SCREEN in obs.observation["available_actions"]:
-      
-      # get player locations
-      player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
-      player_y, player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
-      locationTuple = [int(player_x.mean()), int(player_y.mean())]
 
-      # how close the units will get to the destination before selecting a new target destination
-      distToTarget = 5
-      if ( ((abs(locationTuple[0] - self.target[0]) >= distToTarget) or (abs(locationTuple[1] - self.target[1]) >= distToTarget)) and (self.target[0] >= 0)):
-        # has not reached (or at least gotten close to) destination
-        # no op
-        time.sleep(0.2)
-        print("No op")
-        print("   locationTuple=", locationTuple, "     target=", self.target)
-        return actions.FunctionCall(_NO_OP, [])
-      else:
-        # close enough to destination for a new waypoint
-        # set new target destination waypoint
-        time.sleep(0.2)
-        self.target = self.locations[self.ct % 4] # iterate thru the same 4 array values
+    if _MOVE_SCREEN in obs.observation["available_actions"]:
+      returnObj = self.moveToLocation(obs)
+
+      if returnObj["status"] is "ARRIVED_AT_TARGET":
+        # arrived at target, update next target by incrementing location index (ct)
         self.ct += 1
-        print ("moving to ", self.target[0], ",", self.target[1])
-        return actions.FunctionCall(_MOVE_SCREEN, [_NOT_QUEUED, self.target])
+        if self.ct >= 0:
+
+          # AI API WILL GO HERE TO SET DESTINATION!!!!!
+          self.setTargetDestination(self.locations[self.ct % len(self.locations)])
+
+          print("SETTING new target to: ", self.target)
+        return returnObj["function"]
+
+      elif returnObj["status"] is "MOVING_TO_TARGET":
+        # enroute to target, do not update target
+        return returnObj["function"]
 
     else:
-      print("selecting units")
+      # reset count
+      self.ct = 0
+      print("Starting new simulation.")
       # Select all units
-      # return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
+      print("Selecting units")
+      return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
 
       # Selection Single Unit
-      return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
-
-
-
-class MoveToBeacon(base_agent.BaseAgent):
-  """An agent specifically for solving the MoveToBeacon map."""
-
-  def step(self, obs):
-    super(MoveToBeacon, self).step(obs)
-    if _MOVE_SCREEN in obs.observation["available_actions"]:
-      player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
-      neutral_y, neutral_x = (player_relative == _PLAYER_NEUTRAL).nonzero()
-      if not neutral_y.any():
-        return actions.FunctionCall(_NO_OP, [])
-      target = [int(neutral_x.mean()), int(neutral_y.mean())]
-      return actions.FunctionCall(_MOVE_SCREEN, [_NOT_QUEUED, target])
-    else:
-      return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
-
-
-class CollectMineralShards(base_agent.BaseAgent):
-  """An agent specifically for solving the CollectMineralShards map."""
-
-  def step(self, obs):
-    super(CollectMineralShards, self).step(obs)
-    if _MOVE_SCREEN in obs.observation["available_actions"]:
-      player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
-      neutral_y, neutral_x = (player_relative == _PLAYER_NEUTRAL).nonzero()
-      player_y, player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
-      if not neutral_y.any() or not player_y.any():
-        return actions.FunctionCall(_NO_OP, [])
-      player = [int(player_x.mean()), int(player_y.mean())]
-      closest, min_dist = None, None
-      for p in zip(neutral_x, neutral_y):
-        dist = numpy.linalg.norm(numpy.array(player) - numpy.array(p))
-        if not min_dist or dist < min_dist:
-          closest, min_dist = p, dist
-      return actions.FunctionCall(_MOVE_SCREEN, [_NOT_QUEUED, closest])
-    else:
-      return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
-
-
-class DefeatRoaches(base_agent.BaseAgent):
-  """An agent specifically for solving the DefeatRoaches map."""
-
-  def step(self, obs):
-    super(DefeatRoaches, self).step(obs)
-    if _ATTACK_SCREEN in obs.observation["available_actions"]:
-      player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
-      roach_y, roach_x = (player_relative == _PLAYER_HOSTILE).nonzero()
-      if not roach_y.any():
-        return actions.FunctionCall(_NO_OP, [])
-      index = numpy.argmax(roach_y)
-      target = [roach_x[index], roach_y[index]]
-      return actions.FunctionCall(_ATTACK_SCREEN, [_NOT_QUEUED, target])
-    elif _SELECT_ARMY in obs.observation["available_actions"]:
-      return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
-    else:
-      return actions.FunctionCall(_NO_OP, [])
+      # return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
