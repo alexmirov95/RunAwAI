19dec0da16dd5db4cea438af6f406453d6c7d13e
alexmirov95 <alexander.mirov@gmail.com>
2018-05-21 12:23:12 -0700

    Added movement step function Added a function that takes a varaible step size and a cardinal direction as inputs. Will move all selected unit accordingly. Changed get_location to be a class method. Cleaned up other misc minor parts.

--- pysc2/agents/test_agent_alex.py
+++ pysc2/agents/test_agent_alex.py
@@ -39,29 +39,43 @@ _SELECT_ALL = [0]
 _UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
 
 
-def get_location(obs):
-    # get player locations
-    player_relative = obs.observation["screen"][_PLAYER_RELATIVE]
-    player_y, player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
-    return [int(player_x.mean()), int(player_y.mean())]
-
 class MoveTest(base_agent.BaseAgent):
   ct = 0 # target step count
   target = [0] * 2 # uninitialized target value
   locations = [[int(10), int(10)], [int(60), int(15)], [int(60), int(50)], [int(10), int(50)]]
+  maxMapWidth = 64
+  maxMapHeight = 64
+  obs = None
 
-  def moveToLocation (self, obs):
+  def getCurrentLocation(self):
+      """
+        get player locations
+        returns [xcoord, ycoord] of current location
+      """
+      player_relative = self.obs.observation["screen"][_PLAYER_RELATIVE]
+      player_y, player_x = (player_relative == _PLAYER_FRIENDLY).nonzero()
+      return [int(player_x.mean()), int(player_y.mean())]
+
+  def moveToLocation (self):
+    """
+      Moves selected unit(s) to self.target location. Returns an object with the
+      movement status of the units, i.e. if they're still enroute to target, or
+      if they have arrived, and the return function that actually invokes the 
+      movement. 
+      The returnObject["function"] must be returned in the main step()
+      function to invoke movement.
+    """
     target = self.target
+    sleepTime = 0.03
     # get current location
-    currentLocation = get_location(obs)
+    currentLocation = self.getCurrentLocation()
     # how close the units will get to the destination before selecting a new target destination
     distToTarget = 5
     if (((abs(currentLocation[0] - target[0]) >= distToTarget) or 
         (abs(currentLocation[1] - target[1]) >= distToTarget)) and 
         (self.ct > 0)):
       # has not reached (or at least gotten close to) destination
-      # no op
-      time.sleep(0.2)
+      time.sleep(sleepTime)
       return {
         "function": actions.FunctionCall(_MOVE_SCREEN, [_NOT_QUEUED, target]),
         "status": "MOVING_TO_TARGET"
@@ -69,8 +83,7 @@ class MoveTest(base_agent.BaseAgent):
     else:
       # close enough to destination for a new waypoint
       # set new target destination waypoint
-      print("Arrived at target ", self.target)
-      time.sleep(0.2)
+      time.sleep(sleepTime)
       return {
         "function": actions.FunctionCall(_MOVE_SCREEN, [_NOT_QUEUED, target]),
         "status": "ARRIVED_AT_TARGET"
@@ -82,39 +95,92 @@ class MoveTest(base_agent.BaseAgent):
     """
     x = coordinates[0]
     y = coordinates[1]
-    if ((x > 64) or (0 > x) or 
-      (y > 64) or (0 > y)):
+    if ((x > self.maxMapWidth) or (0 > x) or 
+      (y > self.maxMapHeight) or (0 > y)):
       print("Invalid target coordinates.")
     self.target = [x, y]
 
+
+  def movementStep (self, direction, distance):
+    """
+      Will have selected unit(s) move a specified distnace in the
+      directions "NORTH", "SOUTH", "EAST", "WEST", or "STAY"
+    """
+    newTarget = self.getCurrentLocation()
+
+    if direction is "NORTH":
+      newTarget[1] += distance
+    elif direction is "EAST":
+      newTarget[0] += distance
+    elif direction is "SOUTH":
+      newTarget[1] -= distance
+    elif direction is "WEST":
+      newTarget[0] -= distance
+    elif direction is "NORTHEAST":
+      newTarget[0] += distance
+      newTarget[1] += distance
+    elif direction is "SOUTHEAST":
+      newTarget[0] += distance
+      newTarget[1] -= distance
+    elif direction is "SOUTHWEST":
+      newTarget[0] -= distance
+      newTarget[1] -= distance
+    elif direction is "NORTHWEST":
+      newTarget[0] -= distance
+      newTarget[1] += distance
+
+    elif direction is "STAY":
+      # no movement from current location
+      newTarget = newTarget
+    else:
+      print("Invalid Direction")
+
+    # cap map bounds of new target within map dimensions
+    borderLimit = 4 # target will not be set within borderLimit distance of the edge of map
+    if newTarget[0] >= self.maxMapHeight - borderLimit:
+      newTarget[0] = self.maxMapHeight - borderLimit
+    if newTarget[1] >= self.maxMapWidth - borderLimit:
+      newTarget[1] = self.maxMapWidth - borderLimit
+    if newTarget[0] <= borderLimit:
+      newTarget[0] = borderLimit
+    if newTarget[1] <= borderLimit:
+      newTarget[1] = borderLimit
+
+    self.setTargetDestination(newTarget)
+
+
   def step(self, obs):
     super(MoveTest, self).step(obs)
+    self.obs = obs
 
+    # checks to see if units can move, i.e. if they're selected
     if _MOVE_SCREEN in obs.observation["available_actions"]:
-      returnObj = self.moveToLocation(obs)
+      # call move function
+      returnObj = self.moveToLocation()
 
       if returnObj["status"] is "ARRIVED_AT_TARGET":
         # arrived at target, update next target by incrementing location index (ct)
+
+        
+        # AI API WILL GO HERE TO SET DESTINATION!!!!!
+        # self.setTargetDestination(self.locations[self.ct % len(self.locations)])
+        movementDirection = random.choice(["NORTH","SOUTH", "EAST", "WEST", "NORTHEAST","SOUTHEAST","SOUTHWEST","NORTHWEST","STAY"])
+        stepSize = random.choice(range(1, 25))
+        self.movementStep(movementDirection, stepSize)
+        print("MOVING", movementDirection, stepSize)
+
         self.ct += 1
-        if self.ct >= 0:
-
-          # AI API WILL GO HERE TO SET DESTINATION!!!!!
-          self.setTargetDestination(self.locations[self.ct % len(self.locations)])
-
-          print("SETTING new target to: ", self.target)
         return returnObj["function"]
 
       elif returnObj["status"] is "MOVING_TO_TARGET":
         # enroute to target, do not update target
         return returnObj["function"]
 
+    # select units
     else:
       # reset count
       self.ct = 0
       print("Starting new simulation.")
       # Select all units
-      print("Selecting units")
+      print("Selecting all units")
       return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
-
-      # Selection Single Unit
-      # return actions.FunctionCall(_SELECT_ARMY, [_SELECT_ALL])
